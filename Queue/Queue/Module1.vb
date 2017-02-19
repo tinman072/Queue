f35e3dc007a082b2337aec59b2a5bbc3b45c5ece
tinman072 <nahyan.quasem+github@gmail.com>
2017-02-19 12:45:06 +0600

    Add files via upload

---
+++ Queue/Queue/Module1.vb
@@ -0,0 +1,138 @@
+﻿Module Module1
+
+    Public Structure qNode
+        Dim DATA As String
+        Dim PTR As String
+    End Structure
+
+    Public Structure Queue
+
+        Const nullPTR = -1
+
+        Dim frontPTR As Integer
+        Dim backPTR As Integer
+
+        Dim freelistPTR As Integer
+
+        Dim q() As qNode
+
+        Sub initialize()
+            frontPTR = nullPTR
+            backPTR = nullPTR
+
+            freelistPTR = 0
+
+            ReDim q(9)
+
+            For i = 0 To 8
+                q(i).DATA = ""
+                q(i).PTR = i + 1
+            Next
+
+            q(9).DATA = ""
+            q(9).PTR = nullPTR
+
+        End Sub
+
+        Sub joinQueue(ByVal dataItem As String)
+            If freelistPTR = nullPTR Then
+                Console.WriteLine("No more room in the Queue")
+                Exit Sub
+            End If
+
+            Dim TNP As Integer
+            TNP = freelistPTR
+            q(TNP).DATA = dataItem
+            freelistPTR = q(TNP).PTR
+
+            If frontPTR = nullPTR And backPTR = nullPTR Then
+                frontPTR = TNP
+                backPTR = TNP
+                q(TNP).PTR = nullPTR
+                Exit Sub
+            End If
+
+            q(backPTR).PTR = TNP
+            backPTR = q(backPTR).PTR
+            q(TNP).PTR = nullPTR
+
+
+        End Sub
+
+        Sub leaveQueue()
+            If frontPTR = nullPTR And backPTR = nullPTR Then
+                Console.WriteLine("There are no nodes to remove in the list")
+                Exit Sub
+            End If
+
+            If frontPTR = backPTR Then
+                q(frontPTR).PTR = freelistPTR
+                freelistPTR = frontPTR
+                frontPTR = nullPTR
+                backPTR = nullPTR
+                Exit Sub
+            End If
+
+            Dim TNP As Integer = frontPTR
+            frontPTR = q(frontPTR).PTR
+            q(TNP).PTR = freelistPTR
+            freelistPTR = TNP
+
+        End Sub
+
+        Sub printQueue()
+            If frontPTR = nullPTR And backPTR = nullPTR Then
+                Console.WriteLine("There are no nodes in the Queue")
+                Exit Sub
+            End If
+
+            Dim TNP As Integer = frontPTR
+
+            While TNP <> nullPTR
+                Console.Write(q(TNP).DATA & " ")
+                TNP = q(TNP).PTR
+            End While
+
+            Console.WriteLine("")
+        End Sub
+    End Structure
+
+    Sub Main()
+
+        Dim q As New Queue
+
+        q.initialize()
+
+        q.joinQueue("A")
+        q.joinQueue("B")
+        q.joinQueue("C")
+        q.joinQueue("D")
+
+        q.printQueue()
+
+        q.leaveQueue()
+        q.leaveQueue()
+
+        q.printQueue()
+
+        q.leaveQueue()
+        q.leaveQueue()
+
+        q.printQueue()
+
+        q.joinQueue("W")
+        q.joinQueue("X")
+        q.joinQueue("Y")
+        q.printQueue()
+
+        q.leaveQueue()
+        q.leaveQueue()
+
+        q.printQueue()
+
+
+
+
+    End Sub
+
+End Module
---
+++ Queue/Queue/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Queue")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Queue")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d2a32565-eb02-4991-a34e-867487fa21a1")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
